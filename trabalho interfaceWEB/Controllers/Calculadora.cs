58a64ee3fecda088f0e56a9aadd88d15a7028910
Yasmin <Yasmin@LAPTOP-RQ3HPU0S>
2023-12-03 22:09:26 -0300

    Adicionar arquivos de projeto.

---
+++ trabalho interfaceWEB/Controllers/Calculadora.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using trabalho_interfaceWEB.trabalho_interface;
+
+namespace trabalho_interfaceWEB.Controllers
+{
+    public class Calculadora : Controller, ICalculadora<double>
+    {
+        public double Adicao(double numA, double numB)
+        {
+            return (numA + numB);
+        }
+
+        public double Divisao(double numA, double numB)
+        {
+            if (numB == 0)
+            {
+                throw new ArgumentException("Não é possível dividir por zero.");
+            }
+            return numA / numB;
+        }
+
+        //página inicial da calculadora
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public double Multiplicacao(double numA, double numB)
+        {
+            return (numA * numB);
+        }
+
+        public double Subtracao(double numA, double numB)
+        {
+            return (numA - numB);
+        }
+        public IActionResult Calcular(double operando1, double operando2, string operacao)
+        {
+            double resultado = 0;
+
+            // Realiza a operação selecionada
+            switch (operacao)
+            {
+                case "soma":
+                    resultado = Adicao(operando1, operando2);
+                    break;
+                case "subtracao":
+                    resultado = Subtracao(operando1, operando2);
+                    break;
+                case "multiplicacao":
+                    resultado = Multiplicacao(operando1, operando2);
+                    break;
+                case "divisao":
+                    try
+                    {
+                        resultado = Divisao(operando1, operando2);
+                    }
+                    catch (ArgumentException ex)
+                    {
+                        ViewBag.Erro = ex.Message;
+                        return View("Index");
+                    }
+                    break;
+                default:
+                    resultado = 0;
+                    ViewBag.Erro = "Operação inválida.";
+                    return View("Index");
+            }
+                ViewBag.Resultado = resultado;
+                return View("Index");
+        }
+    }
+}
---
+++ trabalho interfaceWEB/interface.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace trabalho_interfaceWEB
+{
+
+    namespace trabalho_interface
+    { 
+
+     interface ICalculadora<T>
+        {
+            double Adicao(T numA, T numB);
+            double Subtracao(T numA, T numB);
+            double Multiplicacao(T numA, T numB);
+            double Divisao(T numA, T numB);
+            IActionResult Calcular(double operando1, double operando2, string operacao);
+        }
+
+    }
+}
